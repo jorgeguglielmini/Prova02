b357dbdcd434aa79bd0618d4497318ec4fe453a6
jorgeguglielmini <jorge.guglielmini@contmatic.com.br>
2021-07-19 16:43:48 -0300

    #1
    criação da classe enum DDD
    criação do template para fixture factory do telefone
    inclusão das annotation na classe telefone

--- src/main/java/br/com/contmatic/prova/empresa/Telefone.java
+++ src/main/java/br/com/contmatic/prova/empresa/Telefone.java
@@ -1,43 +1,34 @@
 package br.com.contmatic.prova.empresa;
 
-import static br.com.contmatic.prova.empresa.validar.Validacao.assertNotBlank;
-import static br.com.contmatic.prova.empresa.validar.Validacao.verificaEspacoEmBranco;
-import static br.com.contmatic.prova.empresa.validar.Validacao.verificaNulidade;
-import static br.com.contmatic.prova.empresa.validar.Validacao.verificaSomenteNumeros;
-import static br.com.contmatic.prova.empresa.validar.Validacao.verificaTamanhoDdd;
-import static br.com.contmatic.prova.empresa.validar.Validacao.verificaTamanhoNumeroTelefone;
-import static br.com.contmatic.prova.empresa.validar.Validacao.verificaValidadeDdd;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import br.com.contmatic.prova.empresa.enums.DDD;
 
 public class Telefone {
-    
-	private static final String DDD_TEXTO = "DDD";
-
-    private String ddd;
-	
+    	
+	@NotNull
+	@Pattern(regexp = "^[1-9]{1}[0-9]{7,8}", message="Número inválido.") 
 	private String numero;
 	
-	public static final String[] DDD = { "11", "12", "13", "14", "15", "16", "17", "18", "19", "21", "22", "24", "27",
-			"28", "31", "32", "33", "34", "35", "37", "38", "41", "42", "43", "44", "45", "46", "47", "48", "49", "51",
-			"53", "54", "55", "61", "62", "63", "64", "65", "66", "67", "68", "69", "71", "73", "74", "75", "77", "79",
-			"81", "82", "83", "84", "85", "86", "87", "88", "89", "91", "92", "93", "94", "95", "96", "97", "98",
-			"99", };
-
-	public Telefone(String ddd, String numero) {
+	@NotNull
+	private DDD ddd;
+	
+	public Telefone(DDD ddd, String numero) {
 		this.setDdd(ddd);
 		this.setNumero(numero);
 	}
-
-	public String getDdd() {
+	
+	public DDD getDdd() {
 		return ddd;
 	}
 
-	public void setDdd(String ddd) {
-		verificaNulidade(ddd,DDD_TEXTO);
-		assertNotBlank(ddd, DDD_TEXTO);
-		verificaSomenteNumeros(ddd, DDD_TEXTO);
-		verificaEspacoEmBranco(ddd, DDD_TEXTO);
-		verificaTamanhoDdd(ddd);
-		verificaValidadeDdd(ddd);
+	public void setDdd(DDD ddd) {
 		this.ddd = ddd;
 	}
 
@@ -46,48 +37,25 @@ public class Telefone {
 	}
 
 	public void setNumero(String numero) {
-		verificaNulidade(numero,"Número");
-		assertNotBlank(numero, "Número");
-	    verificaEspacoEmBranco(numero, "Número");
-		verificaTamanhoNumeroTelefone(numero);
-		verificaSomenteNumeros(numero, "Número do telefone");
 		this.numero = numero;
 	}
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((ddd == null) ? 0 : ddd.hashCode());
-		result = prime * result + ((numero == null) ? 0 : numero.hashCode());
-		return result;
+		return new HashCodeBuilder().append(this.ddd).append(this.numero).hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Telefone other = (Telefone) obj;
-		if (ddd == null) {
-			if (other.ddd != null)
-				return false;
-		} else if (!ddd.equals(other.ddd))
-			return false;
-		if (numero == null) {
-			if (other.numero != null)
-				return false;
-		} else if (!numero.equals(other.numero))
-			return false;
-		return true;
+		if(obj instanceof Telefone) {
+			Telefone telefone = (Telefone) obj;
+			return new EqualsBuilder().append(this.ddd, telefone.ddd).append(this.numero, telefone.numero).isEquals();
+		}
+		return false;
 	}
 
 	@Override
 	public String toString() {
-		return "Telefone [ddd=" + ddd + ", numero=" + numero + "]";
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
 	}
-
 }
---
+++ src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
@@ -0,0 +1,12 @@
+package br.com.contmatic.prova.empresa.templates;
+
+import br.com.contmatic.prova.empresa.Telefone;
+
+public class TelefoneFixtureFactoryTemplate implements TemplateLoader {
+	
+	public void load() {
+	
+	}
+
+	
+}
