f4a541bf09364a94372e38b598511bc754e0f071
jorgeguglielmini <jorge.guglielmini@contmatic.com.br>
2021-07-19 17:39:43 -0300

    Update TelefoneFixtureFactoryTemplate.java

--- src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
+++ src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
@@ -1,10 +1,11 @@
 package br.com.contmatic.prova.empresa.templates;
 
+import org.apache.commons.lang3.RandomUtils;
+
 import br.com.contmatic.prova.empresa.Telefone;
 import br.com.contmatic.prova.empresa.enums.DDD;
 import br.com.six2six.fixturefactory.Fixture;
-import br.com.six2six.fixturefactory.base.Range;
-import br.com.six2six.fixturefactory.function.impl.RegexFunction;
+import br.com.six2six.fixturefactory.Rule;
 import br.com.six2six.fixturefactory.loader.TemplateLoader;
 
 public class TelefoneFixtureFactoryTemplate implements TemplateLoader {
@@ -12,10 +13,9 @@ public class TelefoneFixtureFactoryTemplate implements TemplateLoader {
 	@Override
 	public void load() {
 		Fixture.of(Telefone.class).addTemplate("valido", new Rule() {{
-			add("ddd", random(DDD.class);
-			add("numero", regex ="^[1-9]{1}[0-9]{7-8}");
+			add("ddd", DDD.values()[RandomUtils.nextInt(0, DDD.values().length)]);
+			add("numero", regex("^[1-9]{1}[0-9]{7-8}"));
 		}});
 	}
 	
-
 }
