1cd3311af9da642f5fde042d7d97b41b0be2151e
jorgeguglielmini <jorge.guglielmini@contmatic.com.br>
2021-07-20 10:05:09 -0300

    criação de templates de testes para telefone

--- src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
+++ src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
@@ -14,7 +14,19 @@ public class TelefoneFixtureFactoryTemplate implements TemplateLoader {
 	public void load() {
 		Fixture.of(Telefone.class).addTemplate("valido", new Rule() {{
 			add("ddd", DDD.values()[RandomUtils.nextInt(0, DDD.values().length)]);
-			add("numero", regex("^[1-9]{1}[0-9]{7-8}"));
+			add("numero", regex("^[1-9]{1}[0-9]{7,8}"));
+		}});
+		
+		Fixture.of(Telefone.class).addTemplate("atributo numero com menos de 7 dígitos").inherits("valido" , new Rule() {{
+			add("numero", regex("^[1-9]{1}[0-9]{1-5}"));
+		}});
+		
+		Fixture.of(Telefone.class).addTemplate("atributo numero com mais de 9 dígitos").inherits("valido" , new Rule() {{
+			add("numero", regex("^[1-9]{1}[0-9]{9,15}"));
+		}});
+		
+		Fixture.of(Telefone.class).addTemplate("atributo numero com digito 0 no início e contendo a quantidade correta de digitos").inherits("valido" , new Rule() {{
+			add("numero", regex("^[0]{1}[1-9]{7,8}"));
 		}});
 	}
 	
