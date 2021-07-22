a87568b3e2f2a67263dc6f5cdb07f9a0cac90c89
jorgeguglielmini <jorge.guglielmini@contmatic.com.br>
2021-07-22 14:09:45 -0300

    criação classe de constantes
    classe de constantes para validacao de regex e mensagens das regex

---
+++ src/main/java/br/com/contmatic/prova/empresa/validar/ConstantesMensagem.java
@@ -0,0 +1,8 @@
+package br.com.contmatic.prova.empresa.validar;
+
+public final class ConstantesMensagem {
+    
+    private ConstantesMensagem() {}
+    
+    public static final String NUMERO_INVALIDO = "Insira um número válido";
+}
---
+++ src/main/java/br/com/contmatic/prova/empresa/validar/ConstantesRegex.java
@@ -0,0 +1,11 @@
+package br.com.contmatic.prova.empresa.validar;
+
+public final class ConstantesRegex {
+
+    private ConstantesRegex() {
+    }
+    
+    
+    public static final String VALIDACAO_NUMERO_TELEFONE = "^[1-9]{1}[0-9]{7,8}";
+    
+}
