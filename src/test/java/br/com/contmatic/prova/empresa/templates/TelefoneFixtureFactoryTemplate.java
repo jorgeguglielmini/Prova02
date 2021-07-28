ea53c485584b2483d7b19f88c33b99f9b0efc434
jorgeguglielmini <jorge.guglielmini@contmatic.com.br>
2021-07-28 09:37:05 -0300

    Update TelefoneFixtureFactoryTemplate.java
    aplicação dos generex na automação dos testes para telefone

--- src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
+++ src/test/java/br/com/contmatic/prova/empresa/templates/TelefoneFixtureFactoryTemplate.java
@@ -2,6 +2,8 @@ package br.com.contmatic.prova.empresa.templates;
 
 import org.apache.commons.lang3.RandomUtils;
 
+import com.mifmif.common.regex.Generex;
+
 import br.com.contmatic.prova.empresa.Telefone;
 import br.com.contmatic.prova.empresa.enums.DDD;
 import br.com.six2six.fixturefactory.Fixture;
@@ -21,43 +23,43 @@ public class TelefoneFixtureFactoryTemplate implements TemplateLoader {
 
         Fixture.of(Telefone.class).addTemplate("atributo numero com menos de 8 dígitos").inherits("valido", new Rule() {
             {
-                add("numero", random("123456", "5689784", "457", "87", "2", "5432", "95128"));
+                add("numero", new Generex("[0-9]{1,7}").random());
             }
         });
 
         Fixture.of(Telefone.class).addTemplate("atributo numero com mais de 9 dígitos").inherits("valido", new Rule() {
             {
-                add("numero", random("1234516789", "23568978412", "457812312122", "1236549871234", "25814796312345", "98765432321423", "3579512832143243"));
+                add("numero", new Generex("[0-9]{10,20}").random());
             }
         });
 
         Fixture.of(Telefone.class).addTemplate("atributo numero com digito 0 no início e contendo a quantidade correta de digitos").inherits("valido", new Rule() {
             {
-                add("numero", random("023456789", "035689784", "05781232", "023654987", "058147963", "08765432", "05795128"));
+                add("numero", new Generex("^[0]{1}[0-9]{7,8}").random());
             }
         });
         
         Fixture.of(Telefone.class).addTemplate("atributo numero com caracteres alfabeticos").inherits("valido", new Rule() {
             {
-                add("numero", random("asssdassd","sfasduih","dhausdhus"));
+                add("numero", new Generex("[a-zA-Z]{8,9}").random());
             }
         });
         
         Fixture.of(Telefone.class).addTemplate("atributo numero com caracteres alfabeticos e numericos").inherits("valido", new Rule() {
             {
-                add("numero", random("a250assd","sfa666uih","d36987hus"));
+                add("numero", new Generex("[a-zA-Z0-9]{8,9}").random());
             }
         });
         
         Fixture.of(Telefone.class).addTemplate("atributo numero com espaco em branco no inicio").inherits("valido", new Rule() {
             {
-                add("numero", random(" 23654715"," 89562314"," 12345678"));
+                add("numero", new Generex("[ 0-9]{8-9}").random());
             }
         });
         
         Fixture.of(Telefone.class).addTemplate("atributo numero com espaco em branco no final").inherits("valido", new Rule() {
             {
-                add("numero", random("23654715 ","89562314 ","12345678 "));
+                add("numero", new Generex("[0-9 ]{8-9}").random());
             }
         });
 
